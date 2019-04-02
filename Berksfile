b596f231fcfe100dd8dac9a3eca6d86fdaae7ba7
root <root@ip-172-31-26-110.ec2.internal>
2019-04-02 19:23:47 +0000

    Adding new tomcat installation files

---
+++ Berksfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+source 'https://supermarket.chef.io'
+
+cookbook 'java_se', '~> 8.131.0' 
+
+metadata
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: tomcat8
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'tomcat8'
+
+service 'tomcat8' do
+  action :start 
+end 
